392e6ecf1233fceec2db3b99b0ff458927d5075a
Pradhumna Guruprasad <59188455+theghoul1827@users.noreply.github.com>
2021-12-14 08:37:43 +0530

    changes url

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const seo = {
   og: {
     title: "Pradhumna Guruprasad Portfolio",
     type: "website",
-    //url: "http://ashutoshhathidara.com/",
+    url: "http://pradhumnaguruprasad.ml/",
   },
 };
 
